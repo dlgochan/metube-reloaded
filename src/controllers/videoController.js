15067b962b0631a9094367de518ba9ffaf96122a
Haechan Lee <dlgockssla@gmail.com>
2022-02-08 13:23:12 +0900

    Finish login part - create login.pug - root routing getLogin, postLogin - implement each controller - npm install bcrypt for compare hashed password values
    Lets get start session

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,7 @@ export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.status(400).render("404", { pageTitle: video.title });
+    return res.status(404).render("404", { pageTitle: video.title });
   }
   return res.render("watch", { pageTitle: video.title, video });
 };
@@ -19,7 +19,7 @@ export const getEdit = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video) {
-    return res.status(400).render("404", { pageTitle: video.title });
+    return res.status(404).render("404", { pageTitle: video.title });
   }
   return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
@@ -29,7 +29,7 @@ export const postEdit = async (req, res) => {
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
-    return res.status(400).render("404", { pageTitle: video.title });
+    return res.status(404).render("404", { pageTitle: video.title });
   }
   await Video.findByIdAndUpdate(id, {
     title,
